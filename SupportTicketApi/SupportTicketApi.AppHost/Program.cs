6217d9714b7dcc654044cd0f72baaff5d42c9b12
Cam Soper <cam.soper@microsoft.com>
2024-04-26 14:28:43 -0500

    updated to preview 6

--- SupportTicketApi/SupportTicketApi.AppHost/Program.cs
+++ SupportTicketApi/SupportTicketApi.AppHost/Program.cs
@@ -2,7 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var sql = builder.AddSqlServer("sql", port: 14329)
                  .AddDatabase("sqldata")
-                 .WithEndpoint(hostPort: 14330);
+                 .WithEndpoint(port: 14330);
 
 builder.AddProject<Projects.SupportTicketApi_ApiModel>("api")
     .WithReference(sql);
